5504b1a249e39f0fb971b48b942b6ef618932d49
nick-pessim <nickolaspessim@hotmail.com>
2020-04-20 23:20:38 -0300

    Finalizado método de login e escopo home

--- rasp_log_system/home/models.py
+++ rasp_log_system/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import os
+import re
 
 # Create your models here.
+class Raspberry(models.Model):
+    nome = models.TextField()
+    ip = models.GenericIPAddressField()
+    vers_snmp = models.TextField()
+    community = models.TextField()
+
+    #Necessário instalação de novas configurações de OID,
+    #assim como a instalação dos pacotes snmp e snmpd
+    def snmpget_pin_temperature(self):
+       response = os.popen("snmpget -v " +self.vers_snmp +" -c " +self.community +" " +str(self.ip) +" 1.3.6.1.4.1.54392.5.118").read()
+       response = response.replace("iso.3.6.1.4.1.54392.5.118 = STRING: ", "")
+       response = re.findall("\d+\.\d+", response)[0]
+       return float(response)
